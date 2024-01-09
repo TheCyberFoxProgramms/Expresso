4bddfaef2b6cf72be3062a232f156ead173f16df
TheAllanProgramm <metro3216@gmail.com>
2024-01-09 03:29:02 +0300

    Work Programm

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 
 
+# tableWidget
+
 class MyWidiget(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -11,24 +13,19 @@ class MyWidiget(QMainWindow):
         self.pushButton.clicked.connect(self.run_btn)
 
     def run_btn(self):
-        title = ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки']
+        def dop_connect(data):
+            per1 = next(cur.execute(f'''SELECT type FROM types WHERE id == '{data[3]}' '''))
+            per2 = next(cur.execute(f'''SELECT roasting FROM roasting WHERE id == '{data[3]}' '''))
+            data = list(data)
+            data[3] = per1[0]
+            data[2] = per2[0]
+            return tuple(data)
+
+        title = ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена',
+                 'объем упаковки']
         con = sqlite3.connect('coffee.sqlite')
         cur = con.cursor()
-        res = list(cur.execute('''SELECT
-                                        id,
-                                        sort,
-                                        roasting.roasting,
-                                        types.type,
-                                        description,
-                                        price,
-                                        volume
-                                     FROM
-                                       coffee
-                                     INNER JOIN types
-                                        ON types.id = coffee.type
-                                     INNER JOIN roasting
-                                          ON roasting.id = coffee.roasting''').fetchall())
-        print(res)
+        res = list(map(dop_connect, cur.execute('''SELECT * FROM coffee''').fetchall()))
         self.tableWidget.setColumnCount(len(title))
         self.tableWidget.setHorizontalHeaderLabels(title)
         self.tableWidget.setRowCount(len(res))
@@ -38,9 +35,6 @@ class MyWidiget(QMainWindow):
         self.tableWidget.resizeColumnsToContents()
 
         con.close()
-
-
-
 
 
 if __name__ == '__main__':
